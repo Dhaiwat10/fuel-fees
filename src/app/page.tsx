d9c9d72cbfbf0f059c194064148943f435db4443
Dhai <dhaiwatpandya@gmail.com>
2024-12-12 02:42:35 +0530

    styling update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,25 @@
 import { ethToUsd } from "@/utils/conversion";
 import { getTransferCost } from "@/utils/transfer";
+import Image from "next/image";
 
 export default async function Home() {
   const gasUsedInTransfer = await getTransferCost();
-  const gasUsedInTransferInUsd = await ethToUsd(+await getTransferCost());
+  const gasUsedInTransferInUsd = await ethToUsd(+(await getTransferCost()));
 
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <h1 className="text-4xl">Fuel Ignition Fees Live</h1>
+    <div className="items-center justify-items-center min-h-screen p-8 pb-20 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+      <main className="flex flex-col row-start-2 items-center sm:items-start">
+        <Image src="/fuel_logo.svg" alt="fuel logo" width={100} height={100} />
 
-        <h3>Cost for a transfer: ${gasUsedInTransferInUsd} ({gasUsedInTransfer.toString()} ETH)</h3>
+        <h1 className="text-4xl mt-2">Fuel Ignition Fees Live</h1>
+
+        <span className="mt-8">
+          Cost for a transfer
+        </span>
+
+        <h3 className="text-5xl font-bold mt-1">${gasUsedInTransferInUsd}</h3>
+
+        <span>{gasUsedInTransfer.toString()} ETH</span>
       </main>
     </div>
   );
